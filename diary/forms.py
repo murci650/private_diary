2b1992562fdc82af5c7310f58adf710157859a27
first commit <>
2023-08-28 19:05:06 +0900

    inquiry

--- diary/forms.py
+++ diary/forms.py
@@ -1,10 +1,38 @@
+import os
+
 from django import forms
+from django.core.mail import EmailMessage
+#django.coe.mail.Email.EmailMessageクラスを使ってメール送信する
+#このクラスはBccやメール添付も行うことができる。取扱いが難しいこともない。
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前',max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル',max_length=30)
     message = forms.CharField(label='メッセージ',widget=forms.Textarea)
+
+    def send_email(self):
+        name = self.cleaned_data['name']<!-- 2-->
+#引数のselfから「self.cleaned_data'<フィールド名>'のようにして、前節のビューの時と同様にフォームバリデーションを通ったユーザ入力値を取得できる
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ{}'.format(title)
+        message = '送信者名:{0}?\nメールアドレス:{1}\nメッセージ:\n{2}'.format(name, email, message)
+        from_email = os.environ.get('FROM_EMAIL')
+#送信元(from_email)と宛先(to_list)は、settings.pyで設定されたデータベースパスワードのように環境変数からセットする
+        to_list = [
+            os.environ.get('FROM_EMAIL')
+#送信元from_emailと宛先to_listはsettings.pyで設定したデータベースのパスワードのように環境変数からセットする。
+#ただし開発環境では、これらの値がブランクでも動作に支障がないためPyCharmで環境変数を設定している
+        ]
+        cc_list = [
+            email
+        ]
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)<!-- 4--><!-- -->
+        message.send()<!-- 5--><!-- -->
+#EmailMessageインスタンスを作成して、sendメソッドを呼び出す
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,3 +48,8 @@ class InquiryForm(forms.Form):
 
 # __init__メソッドの中でself.fieldを編集することでcss属性を操作している
 # 操作内容はBootstrapのフォーム用クラスform-controlとplaceholderを追加している
+
+
+
+
+
--- diary/views.py
+++ diary/views.py
@@ -1,10 +1,32 @@
+import logging
+
+from django.urls import reverse_lazy
+#URLを指定する際はreverse_lazy関数でURLのハードコーティングを避ける。
+#reverse_lazy('<urls.pyのapp_name>:<ルーティングにつけたname>')
+
 from django.shortcuts import render
 from django.views import generic
 from .forms import InquiryForm
 
+logger = logging.getLogger(__name__)
+#loggerを取得
+#logger.<ログレベル>(<出力内容>]
+
 class IndexView(generic.TemplateView):
-    template_name = "Index.html"
+    template_name = "index.html"
 
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+    #FormViewはクラス変数にURLを指定するだけで、処理に問題が無かった時に指定URLにリダイレクトする
+
+def form_valid(self, form):
+#フォームバリデーションに問題なければ実行されるメソッド
+#フォームバリデーションを通ったユーザ入力値を↓の形でとりだせる
+#form.cleaded_data['<フィールド名>']
+    form.send_email()
+    #form.pyにメール送信メソッドを呼び出す
+    logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+    #ビューからログを出力
+    return super().form_valid(form)
